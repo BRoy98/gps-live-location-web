6e27f42227ed16bd75653752092d679a9f87eb7b
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2020-03-02 13:45:06 +0530

    Fix multiple device tracking
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,68 @@
-window.lat = 31.251634;
-window.lng = 75.705436;
-window.lat2 = 31.258291;
-window.lng2 = 75.709402;
+window.lat = 31.251127;
+window.lng = 75.705351;
+window.lat2 = 31.251127;
+window.lng2 = 75.705351;
 var map;
 var mark;
 var mark2;
 var flightPath;
 var flightPath2;
 var locationTrack = [{
-    lat: 31.251634,
-    lng: 75.705436
-}, {
-    lat: 31.247600,
-    lng: 75.697349
-}, {
-    lat: 31.251503,
-    lng: 75.705816
-}]
-
+    lat: 31.251127,
+    lng: 75.705351
+}];
 var locationTrack2 = [{
-    lat: 31.251625,
-    lng: 75.705420
-}, {
-    lat: 31.251542,
-    lng: 75.705632
-}, {
-    lat: 31.258291,
-    lng: 75.709402
+    lat: 31.251127,
+    lng: 75.705351
 }];
 
 function getLocation() {
+    axios({
+            method: 'post',
+            url: 'https://api.iotsardar.com/v1/user/get/text',
+            data: {
+                "api_key": "1f178a1a3bc6ac3a97994e00cd775672",
+                "widget_id": "aa80dfd932a54256"
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+            locationData = response.data.state;
+            locationData = locationData.split('|');
+            console.log(Number(locationData[0]), Number(locationData[1]));
+            if (locationTrack[locationTrack.length - 1].lat != Number(locationData[0]) ||
+                locationTrack[locationTrack.length - 1].lng != Number(locationData[1])) {
+
+                redraw(Number(locationData[0]), Number(locationData[1]));
+
+                locationTrack.push({
+                    lat: Number(locationData[0]),
+                    lng: Number(locationData[1])
+                });
+
+                console.log(1, locationTrack);
+            }
+
+            // var curPath = flightPath.getPath();
+            // curPath.push(new google.maps.LatLng(Number(locationData[0]), Number(locationData[1])));
+            flightPath.setPath(locationTrack);
+
+            // console.log(1, curPath);
+
+            setTimeout(function () {
+                getLocation();
+            }, 5000);
+        })
+        .catch(function (response) {
+            console.log(response);
+        });
+    return null;
+};
+
+function getLocation2() {
     axios({
             method: 'post',
             url: 'https://api.iotsardar.com/v1/user/get/text',
@@ -46,51 +79,25 @@ function getLocation() {
             locationData = response.data.state;
             locationData = locationData.split('|');
             console.log(Number(locationData[0]), Number(locationData[1]));
-            if (locationTrack[locationTrack.length - 1].lat != Number(locationData[0]) ||
-                locationTrack[locationTrack.length - 1].lng != Number(locationData[1])) {
-                locationTrack.push({
-                    lat: Number(locationData[0]),
-                    lng: Number(locationData[1])
-                })
-                redraw(Number(locationData[0]), Number(locationData[1]));
-                console.log('locationTrack', locationTrack);
-            }
-        })
-        .catch(function (response) {
-            console.log(response);
-        });
-    return null;
-};
-
-function getLocation2() {
-    axios({
-            method: 'post',
-            url: 'https://api.iotsardar.com/v1/user/get/text',
-            data: {
-                "api_key": "1f178a1a3bc6ac3a97994e00cd775672",
-                "widget_id": "aa80dfd932a54256"
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(function (response) {
-            locationData = response.data.state;
-            locationData = locationData.split('|');
-            console.log(Number(locationData[0]), Number(locationData[1]));
-
             if (locationTrack2[locationTrack2.length - 1].lat != Number(locationData[0]) ||
                 locationTrack2[locationTrack2.length - 1].lng != Number(locationData[1])) {
+                redraw2(Number(locationData[0]), Number(locationData[1]));
+
                 locationTrack2.push({
                     lat: Number(locationData[0]),
                     lng: Number(locationData[1])
-                })
-                redraw(Number(locationData[0]), Number(locationData[1]));
-                console.log('locationTrack2', locationTrack2);
+                });
+                console.log(2, locationTrack2);
             }
 
+            // var curPath = flightPath2.getPath();
+            // curPath.push(new google.maps.LatLng(Number(locationData[0]), Number(locationData[1])));
+            flightPath2.setPath(locationTrack2);
+
+            // console.log(2, locationTrack2);
+
             setTimeout(function () {
-                getLocation2(location);
+                getLocation2();
             }, 5000);
         })
         .catch(function (response) {
@@ -99,10 +106,10 @@ function getLocation2() {
     return null;
 };
 
-setInterval(function () {
-    getLocation();
-    getLocation2();
-}, 5000);
+// setInterval(function () {
+getLocation();
+getLocation2();
+// }, 5000);
 
 
 var initialize = function () {
@@ -119,7 +126,6 @@ var initialize = function () {
         new google.maps.Point(0, 0),
         new google.maps.Point(32, 32));
 
-
     var bikeImage = new google.maps.MarkerImage('/images/motorbike.png',
         new google.maps.Size(64, 64),
         new google.maps.Point(0, 0),
@@ -131,15 +137,16 @@ var initialize = function () {
             lat: lat,
             lng: lng
         },
-        icon: markerImage,
+        icon: bikeImage,
         map: map
     });
+
     mark2 = new google.maps.Marker({
         position: {
             lat: lat2,
             lng: lng2
         },
-        icon: bikeImage,
+        icon: markerImage,
         map: map
     });
 
@@ -151,6 +158,7 @@ var initialize = function () {
         strokeWeight: 5,
         map: map
     });
+
     flightPath2 = new google.maps.Polyline({
         path: locationTrack2,
         geodesic: true,
@@ -159,37 +167,28 @@ var initialize = function () {
         strokeWeight: 5,
         map: map
     });
-
 };
 
-function redraw(alat, alng) {
+function redraw(lat, lng) {
     if ($("#id-name--1").is(":checked"))
         map.setCenter({
-            lat: alat,
-            lng: alng,
+            lat: lat,
+            lng: lng,
             alt: 0
         });
     mark.setPosition({
-        lat: alat,
-        lng: alng,
+        lat: lat,
+        lng: lng,
         alt: 0
     });
-    flightPath.setPath(locationTrack);
 }
 
-function redraw2(mlat, mlng) {
-    if ($("#id-name--1").is(":checked"))
-        map.setCenter({
-            lat: mlat,
-            lng: mlng,
-            alt: 0
-        });
+function redraw2(lat, lng) {
     mark2.setPosition({
-        lat: mlat,
-        lng: mlng,
+        lat: lat,
+        lng: lng,
         alt: 0
     });
-    flightPath2.setPath(locationTrack2);
 }
 
 window.initialize = initialize;
